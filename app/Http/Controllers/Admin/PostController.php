a5aeb8165786b7746850423549832f8d2931ee85
EdisonDavidCode <edisondavidr.rd@gmail.com>
2023-10-29 15:18:19 -0400

    Formulario para crear nuevo post - Aprende a crear un sistema de blog con Laravel 10

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
@@ -23,7 +25,9 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::pluck('name','id');
+        $tags = Tag::all();
+        return view('admin.posts.create',compact('categories','tags'));
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
+    <a href="{{ route('admin.posts.create') }}" class="btn btn-success float-right mr-4">Nuevo Post</a>
     <h1>Listado de post</h1>
 @stop
 
